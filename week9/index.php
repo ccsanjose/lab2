0a1c1fc3aaa41680e84b565920170ead28310017
ccsanjose <119903985+ccsanjose@users.noreply.github.com>
2023-02-07 21:44:12 +0800

    Update index.php

--- week9/index.php
+++ week9/index.php
@@ -315,6 +315,24 @@ echo $gender;
 
 ?>
 
+ <script>
+  /*loop content*/
+ /*iterables content*/
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
+ 
  <?php
  if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $servername = "localhost";
@@ -343,23 +361,7 @@ $conn->close();
 ?>
 </div>
 
-<script>
-  /*loop content*/
- /*iterables content*/
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-</script>
+
 
 
 
