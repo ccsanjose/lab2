a205ea6e7ff91449ad8269ac44903fc7e58871a9
ccsanjose <119903985+ccsanjose@users.noreply.github.com>
2023-02-07 22:14:34 +0800

    Update index.php

--- week9/index.php
+++ week9/index.php
@@ -334,8 +334,7 @@ echo $gender;
 
 ?>
  
- <?php
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -345,20 +344,19 @@ $dbname = "mydbsj";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("<br>Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('$name', '', '$email')";
+VALUES ('Juan', 'Dela Cruz', 'delacruz@gmail.com')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "<br>New record created successfully";
+  echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
- 
+
 $conn->close();
- }
 ?>
 </div>
 
